7125b0a6b923a96d2b66f88733cc9cc0f979447f
samuele reghenzi <sreghenzi@gitlab.eng.it>
2017-10-05 16:02:12 +0200

    Initial commit

---
+++ src/main/scala/org/webdebs/SimpleApp.scala
@@ -0,0 +1,18 @@
+package org.webdebs
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "./README.md" // Should be some file on your system
+
+    val spark = SparkSession.builder.appName("Simple Application").master("local[2]").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}

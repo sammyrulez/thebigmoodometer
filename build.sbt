aea49c6e3abecbe70379347e62258d7eed6b198f
samuele reghenzi <sreghenzi@gitlab.eng.it>
2017-10-11 21:00:46 +0200

    Basic spark

--- build.sbt
+++ build.sbt
@@ -4,7 +4,13 @@ version <<= version in ThisBuild
 
 scalaVersion := "2.11.8"
 
+val configVersion = "1.3.0"
+
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+
+libraryDependencies ++= Seq(
+  "com.typesafe" % "config" % configVersion
+)
 
 publishTo := Some(Resolver.file("file",  new File( Path.userHome.absolutePath+"/my-maven-repo" )) )
 
--- src/main/scala/org/webdebs/SimpleApp.scala
+++ src/main/scala/org/webdebs/SimpleApp.scala
@@ -1,18 +1,29 @@
 package org.webdebs
 
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.spark.serializer.KryoSerializer
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
 
+import scala.io.Source
+
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "./README.md" // Should be some file on your system
 
-    val spark = SparkSession.builder.appName("Simple Application").master("local[2]").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
+    val conf: Config = ConfigFactory.load("application.conf")
 
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+
+    val stopWords =  Source.fromInputStream(getClass.getResourceAsStream(conf.getString("stopWordsFile"))).getLines().toList
+
+    val spark = SparkSession.builder.appName("Simple Application")
+     // .master("local[2]")
+      .master(conf.getString("sparkUrl"))
+      .config("spark.serializer", classOf[KryoSerializer].getCanonicalName)
+      .config("spark.jars", conf.getString("appClasses"))
+      .getOrCreate()
+
+
+
     spark.stop()
   }
 }
